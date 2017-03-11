8a078e7a30b2d1fdeb2d648bfc89f13a063fbeb5
HanYangZhao <han.y.zhao@mail.mcgill.ca>
2017-03-11 03:30:12 -0500

    added mqtt

--- Liquid_Level_Sensor/Liquid_Level_Sensor.ino
+++ Liquid_Level_Sensor/Liquid_Level_Sensor.ino
@@ -1,17 +1,33 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+#include <DNSServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
 int water_ain=0;
 int ad_value=0;
 
 
-WiFiClient client;
-const char* ssid = "";
-const char* password = "";
+WiFiClient espclient;
+WiFiManager wifiManager;
 bool isConnected = false;
+
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "192.168.2.212"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    ""
+#define AIO_KEY         ""
+#define AIO_TOPIC       "/homeassistant/basement/WaterLevelSensor/level"
+#define DEVICE_ID       "WaterLevelSensor"
+
+Adafruit_MQTT_Client mqtt(&espclient, AIO_SERVER, AIO_SERVERPORT);
+Adafruit_MQTT_Publish waterlevel = Adafruit_MQTT_Publish(&mqtt,AIO_TOPIC);
 
 unsigned long current_millis;
 unsigned long previous_millis = 0;
-unsigned long detect_interval = 900000;
+unsigned long detect_interval = 300000;
 
 const char thingSpeakAddress[] = "api.thingspeak.com";
 const char thingSpeakAPIKey[] = "";
@@ -20,60 +36,7 @@ const char thingSpeakAPIKey[] = "";
 const String sendNumber = "";
 const String sms_key = "";
 
-boolean wifiConnect(){
- //wait 1 minute for wifi connection
- WiFi.begin(ssid, password);
- for (int i = 0 ; i < 60 ; i++){
-  if (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
-  } 
-  else{
-    return true;
-  }
- }
- return false;
-}
-
-void sendSMS(String number,String message)
-{
-  // Make a TCP connection to remote host
-  if (client.connect(thingSpeakAddress, 80))
-  {
-
-    //should look like this...
-    //api.thingspeak.com/apps/thinghttp/send_request?api_key={api key}&number={send to number}&message={text body}
-
-    client.print("GET /apps/thinghttp/send_request?api_key=");
-    client.print(sms_key);
-    client.print("&number=");
-    client.print(number);
-    client.print("&message=");
-    client.print(message);
-    client.println(" HTTP/1.1");
-    client.print("Host: ");
-    client.println(thingSpeakAddress);
-    client.println("Connection: close");
-    client.println();
-  }
-  else
-  {
-    Serial.println(F("Connection failed"));
-  } 
-
-  // Check for a response from the server, and route it
-  // out the serial port.
-  while (client.connected())
-  {
-    if ( client.available() )
-    {
-      char c = client.read();
-      Serial.print(c);
-    }      
-  }
-  Serial.println();
-  client.stop();
-}
+void MQTT_connect();
 
 String URLEncode(const char* msg)
 {
@@ -99,51 +62,95 @@ String URLEncode(const char* msg)
 //update data to thingSpeak
 void updateThingSpeak(String tsData) {
 
-  if (!client.connect(thingSpeakAddress, 80)) {
+  if (!espclient.connect(thingSpeakAddress, 80)) {
      return;
   }
-  client.print(F("GET /update?key="));
-  client.print(thingSpeakAPIKey);
-  client.print(F("&"));
-  client.print(tsData);
-  client.print(F(" HTTP/1.1\r\nHost: api.thingspeak.com\r\n\r\n"));
-  client.println();
+  espclient.print(F("GET /update?key="));
+  espclient.print(thingSpeakAPIKey);
+  espclient.print(F("&"));
+  espclient.print(tsData);
+  espclient.print(F(" HTTP/1.1\r\nHost: api.thingspeak.com\r\n\r\n"));
+  espclient.println();
   Serial.println("uploaded to think speak");
+}
+
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(3000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
+
+
+void updateMQTT(char* data){
+  mqtt.disconnect();
+  MQTT_connect();
+  if (!waterlevel.publish(data)) {
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("OK!"));
+  }
 }
 
 
 void waterDetection(){
-   current_millis = millis();
+  current_millis = millis();
   if(current_millis - previous_millis >= detect_interval) {
     ad_value=analogRead(water_ain);
     if(ad_value>200)
     {
       Serial.println("High water level");
+      //mqtt.disconnect();
       updateThingSpeak("1=1");
-      sendSMS(sendNumber, URLEncode("High water level in well"));
+      delay(5000);
+      updateMQTT("Low");
     }
     else
     {
       Serial.println("Low water level");
+      //mqtt.disconnect();
       updateThingSpeak("1=0");
+      delay(5000);
+      updateMQTT("Low");
     }
     previous_millis = current_millis;
   }
 }
 
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+
+void callback(char* topic, byte* payload, unsigned int length){
+  
+}
 void setup()
 {
   Serial.begin(9600);
   Serial.println();
   Serial.print("connecting to ");
-  Serial.println(ssid);
-  isConnected = wifiConnect();
+  wifiManager.autoConnect("WaterLevelSensor");
+  //setup_wifi();
+  MQTT_connect();
 }
 void loop(){
-  if(!isConnected){
-    wifiConnect();
-  }
-  else{
-    waterDetection();
-  }
+  waterDetection();
+ 
 }
